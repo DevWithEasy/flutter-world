ab17e12d4b1f270c421afd64f5a420aac1453633
DevWithEasy <devwitheasy@gmail.com>
2025-01-15 23:01:04 +0600

    permission add

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'package:flutter_world/app/app.dart';
 import 'package:permission_handler/permission_handler.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Permission.storage.request();
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Permission.storage.request();
   runApp(FlutterWorldApp());
 }
